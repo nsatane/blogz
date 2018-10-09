f6fb81f50fa03cba6f1a1a7e6e48dcb5a30248bb
Neha Bhalke <nsatane@gmail.com>
2018-10-08 20:32:15 -0500

    Final

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template 
+from flask import Flask, request, redirect, render_template,flash
 from flask_sqlalchemy import SQLAlchemy
 import cgi
 
@@ -22,64 +22,63 @@ class Blog(db.Model):
         self.body = body
     
     
-
-
-@app.route('/')
-def index():
-    return render_template('blog.html') 
-
+     
 # route to main blog page
 @app.route('/blog')
 def blog_index():
     # args getting id dictionary element from table
-    blog_id = request.args.get('id')
-    #blog = Blog.query.all()
-
-    if blog_id:
+    if request.args:
+        blog_id = request.args.get('id')
+        #blog = Blog.query.all()
         post = Blog.query.get(blog_id)
         blog_title = post.title
         blog_body = post.body
         # Use Case 1: Click on a blog entry's title on the main page 
         # and go to a blog's individual entry page.
-        return render_template('entry.html', title="Blog Entry #" 
-        + blog_id, blog_title=blog_title, blog_body=blog_body)
-
+        return render_template('entry.html', title="Blog Title" + blog_id, blog_title=blog_title, blog_body=blog_body)
+    else:
+        blogs = Blog.query.all()
+        return render_template('blog.html', title="Add a new Blog", blogs=blogs)
 
 #handler route to new posts
-@app.route('/newpost')
-def new_post():
-    return render_template('newpost.html', title="Add New Blog Entry")
 
 #handler route to verify new post and body
 @app.route('/newpost', methods=['GET','POST'])
 def verify_post():
-    
-    blog_title = request.form['title']
-    blog_body = request.form['body']
-    title_error = ''
-    body_error = ''
-    new_blog = Blog(blog_title, blog_body)
+    if request.method == "GET":
+        return render_template("newpost.html")
+
+    if request.method == 'POST':
+        blog_title = request.form['title']
+        blog_body = request.form['body']
+        title_error = ""
+        body_error = ""
+        new_blog = Blog(blog_title, blog_body)
 
     #error validation massage for empty fields
-    if blog_title == "":
-        title_error = "Please Enter Title for your Blog."
-    elif blog_body == "":
-        body_error = "Fill in the Body for your Blog."
-    
-    #add a new blog post and commit it to new id:
-    if not title_error and not body_error:
-        db.session.add(new_blog)
-        db.session.commit()
-        blog = new_blog.id
-    #User Case2: After adding a new blog post, instead of going back to the main page, 
-    # we go to that blog post's individual entry page. Redirect to specific blog id page.
-        return redirect('/blog?id={0}'.format(blog))
-    else:
-        # return user to post page with errors.
-        return render_template('newpost.html', title="Add New Blog Entry", 
-        blog_title = blog_title, blog_body = blog_body, 
-        title_error = title_error, body_error = body_error)
+        if blog_title == "":
+            title_error = "Please Enter Title for your Blog."
+        if blog_body == "":
+            body_error = "Fill in the Body for your Blog."
+        
+        #add a new blog post and commit it to new id:
+        if not title_error and not body_error:
+            new_blog = Blog(blog_title, blog_body)
+            db.session.add(new_blog)
+            db.session.commit()
+            blog = new_blog.id
+        #User Case2: After adding a new blog post, instead of going back to the main page, 
+        # we go to that blog post's individual entry page. Redirect to specific blog id page.
+            return redirect('/blog?id={0}'.format(blog))
+        if title_error or body_error:
+            # return user to post page with errors.
+            return render_template('newpost.html', title="Add a New Post", 
+            blog_title = blog_title, blog_body = blog_body, 
+            title_error = title_error, body_error = body_error)
 
+@app.route('/')
+def index():
+    return redirect('/blog')
     
 
 if __name__ == '__main__':
